a49ae3fce37a05536127635a3b9c533a6c029d28
ramz <aly.ranzam@gmail.com>
2026-08-05 09:41:58 +0545

    clean up boiler plate codes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Hello MayaXpress</h1>
+      <h1 className="text-2xl font-bold text-blue-700">Hello MayaXpress</h1>
     </div>
   );
 }
